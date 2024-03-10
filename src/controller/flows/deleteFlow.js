95092a594aa1a62772901d64b533d5ca479d8352
RyLai <ryanlai@kkcompany.com>
2024-03-10 20:18:03 +0800

    refactor(Flowgrid.js): fix grid size css

--- src/controller/flows/deleteFlow.js
+++ src/controller/flows/deleteFlow.js
@@ -1,22 +1,31 @@
 import database from '../sqlite.js'
 
-const deleteFlows = (event, id) => {
+const deleteFlow = (event, id) => {
   const deleteFlowStmt = database.prepare('DELETE FROM flows WHERE id = ?')
   const deleteFlowNodesStmt = database.prepare(
     'DELETE FROM flow_nodes WHERE flow_id = ?',
   )
 
   database.transaction(() => {
-    deleteFlowNodesStmt.run(id)
-    const info = deleteFlowStmt.run(id)
-
-    if (info.changes > 0) {
-      console.log(`Flow with id ${id} was successfully deleted.`)
-      return true
+    try {
+      deleteFlowNodesStmt.run(id)
+    } catch (error) {
+      console.error(`Error deleting nodes for flow with id ${id}: ${error}`)
     }
+
+    try {
+      const info = deleteFlowStmt.run(id)
+      if (info.changes > 0) {
+        console.log(`Flow with id ${id} was successfully deleted.`)
+        return true
+      }
+    } catch (error) {
+      console.error(`Error deleting flow with id ${id}: ${error}`)
+    }
+
     console.log(`No flow found with id ${id}.`)
     return false
   })()
 }
 
-export default deleteFlows
+export default deleteFlow
