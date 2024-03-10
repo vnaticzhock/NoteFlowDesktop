62f9a5bd09edb1f1787723fbef2f2d252e51ba6a
RyLai <ryanlai@kkcompany.com>
2024-03-10 20:59:27 +0800

    fix: pagetab and flow synchronize delete

--- src/components/PageTab/PageTab.jsx
+++ src/components/PageTab/PageTab.jsx
@@ -3,50 +3,18 @@ import './PageTab.scss'
 import Stack from '@mui/material/Stack'
 import React from 'react'
 import { FaBook, FaPen } from 'react-icons/fa'
-import { useNavigate } from 'react-router-dom'
 
-import { createFlow } from '../../apis/APIs.jsx'
 import { ButtonGroup, IconButton, Toolbar } from '../Common/Mui.jsx'
 import { DeleteIcon, PlusIcon } from '../Common/ReactIcon'
 
 export default function PageTab({
   tabList,
-  setTabList,
   toFlow,
   activeTab,
-  setActiveTab,
+  addNewTab,
+  removeTab,
 }) {
   const MaxTitleLen = 10
-  const navigate = useNavigate()
-  const addNewTab = async () => {
-    try {
-      const flow = await createFlow()
-      setTabList([...tabList, flow])
-      toFlow(flow)
-    } catch (error) {
-      console.error('Error creating flow:', error)
-    }
-  }
-  const removeTab = (id) => {
-    const indexToDelete = tabList.findIndex((tab) => tab.id === id)
-
-    if (indexToDelete === -1) {
-      console.error(`Tab with ID ${id} not found.`)
-    }
-
-    const filteredTabs = tabList.filter((tab) => tab.id !== id)
-    const newActiveTabIndex = indexToDelete - 1
-
-    setTabList(filteredTabs)
-
-    if (newActiveTabIndex !== -1) {
-      setActiveTab(filteredTabs[newActiveTabIndex].id)
-      toFlow(filteredTabs[newActiveTabIndex])
-    } else {
-      setActiveTab(-1)
-      navigate('/')
-    }
-  }
 
   return (
     <Toolbar className="toolbar" direction="row" spacing={2}>
